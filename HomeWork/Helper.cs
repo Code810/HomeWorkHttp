dd9e82bc6e9354bf0ea6e16d0aecf8f4ec37a3d2
code810 <nadirssh@code.edu.az>
2024-01-15 10:36:24 +0400

    task ready

---
+++ HomeWork/Helper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    public class Helper
+    {
+        public async Task<List<User>> GetHttpData(Predicate<User> predicate)
+        {
+            HttpClient httpClient = new HttpClient();
+            string apiUrl = "https://jsonplaceholder.typicode.com/posts";
+            HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+
+                var deptList = JsonSerializer.Deserialize<List<User>>(responseContent);
+                var newlist = deptList.FindAll(predicate);
+                return newlist;
+            }
+            else return null;
+        }
+            
+        public  void AddDataFile(List<User> data)
+        {
+           
+                string strserialize = JsonSerializer.Serialize(data);
+            string fileName = "Data.txt";
+                string filepath = "C:\\Users\\seidb\\OneDrive\\Masaüstü\\Projects";
+                string fullFilePath = Path.Combine(filepath, fileName);
+                if (!File.Exists(fullFilePath))
+                {
+                   File.Create(fullFilePath);
+                    File.WriteAllText(fullFilePath, strserialize);
+                }
+                else
+                {
+                    File.WriteAllText(fullFilePath, strserialize);
+                }
+                Console.WriteLine("Adding text to file successful");
+          
+          
+        }
+
+   
+    }
+}
---
+++ HomeWork/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using HomeWork;
+
+Helper helper = new Helper();
+
+var  Data=await helper.GetHttpData(d=>d.id==1||d.id==10||d.id==100);
+
+
+ helper.AddDataFile(Data);
